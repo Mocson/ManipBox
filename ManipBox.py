25a4f044a46627c6524774ef31d01e4d717d9d7a
Mocson <kimura.ryota.as@gmail.com>
2018-09-26 14:35:49 +0900

    Widthの調整

--- ManipBox.py
+++ ManipBox.py
@@ -27,28 +27,28 @@ def ManiWrdRotate():
 def ManiCusRotate():
     cmds.manipRotateContext("Rotate", e=True, m=6)
 
-cmds.window(title='ManipBox', w=150)
+cmds.window(title='ManipBox', w=200)
 cmds.columnLayout(adj=True)
 
 cmds.separator( h=3 )
 cmds.text("World")
 cmds.separator( h=3 )
-cmds.button( label = 'Object', w=100, h=20, command='ManiObjMove()')
-cmds.button( label = 'World', w=100, h=20, command='ManiWrdMove()')
-cmds.button( label = 'Custom', w=100, h=20, command='ManiCusMove()')
+cmds.button( label = 'Object', w=100, h=30, command='ManiObjMove()')
+cmds.button( label = 'World', w=100, h=30, command='ManiWrdMove()')
+cmds.button( label = 'Custom', w=100, h=30, command='ManiCusMove()')
 
 cmds.separator( h=3 )
 cmds.text("Scale")
 cmds.separator( h=3 )
-cmds.button( label = 'Object', w=100, h=20, command='ManiObjScale()')
-cmds.button( label = 'World', w=100, h=20, command='ManiWrdScale()')
-cmds.button( label = 'Custom', w=100, h=20, command='ManiCusScale()')
+cmds.button( label = 'Object', w=100, h=30, command='ManiObjScale()')
+cmds.button( label = 'World', w=100, h=30, command='ManiWrdScale()')
+cmds.button( label = 'Custom', w=100, h=30, command='ManiCusScale()')
 
 cmds.separator( h=3 )
 cmds.text("Rotate")
 cmds.separator( h=3 )
-cmds.button( label = 'Object', w=100, h=20, command='ManiObjRotate()')
-cmds.button( label = 'World', w=100, h=20, command='ManiWrdRotate()')
-cmds.button( label = 'Custom', w=100, h=20, command='ManiCusRotate()')
+cmds.button( label = 'Object', w=100, h=30, command='ManiObjRotate()')
+cmds.button( label = 'World', w=100, h=30, command='ManiWrdRotate()')
+cmds.button( label = 'Custom', w=100, h=30, command='ManiCusRotate()')
 
 cmds.showWindow()

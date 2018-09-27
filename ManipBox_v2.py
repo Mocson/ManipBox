8b03587ceeff352d1f93676e2a8c2a3c0851755f
Mocson <kimura.ryota.as@gmail.com>
2018-09-27 10:39:40 +0900

    修正

--- ManipBox_v2.py
+++ ManipBox_v2.py
@@ -1,32 +1,29 @@
 import maya.cmds as cmds
 
-def ManipBox():
-    def wrd():
-        cmds.manipMoveContext("Move", e=True, m=2)
-        cmds.manipScaleContext("Scale", e=True, m=2)
-        cmds.manipRotateContext("Rotate", e=True, m=2)
+def wrd():
+    cmds.manipMoveContext("Move", e=True, m=2)
+    cmds.manipScaleContext("Scale", e=True, m=2)
+    cmds.manipRotateContext("Rotate", e=True, m=2)
 
-    def lcl():
-        cmds.manipMoveContext("Move", e=True, m=0)
-        cmds.manipScaleContext("Scale", e=True, m=0)
-        cmds.manipRotateContext("Rotate", e=True, m=0)
+def lcl():
+    cmds.manipMoveContext("Move", e=True, m=0)
+    cmds.manipScaleContext("Scale", e=True, m=0)
+    cmds.manipRotateContext("Rotate", e=True, m=0)
 
-    def cus():
-        cmds.manipMoveContext("Move", e=True, m=6)
-        cmds.manipScaleContext("Scale", e=True, m=6)
-        cmds.manipRotateContext("Rotate", e=True, m=6)
+def cus():
+    cmds.manipMoveContext("Move", e=True, m=6)
+    cmds.manipScaleContext("Scale", e=True, m=6)
+    cmds.manipRotateContext("Rotate", e=True, m=6)
 
-    cmds.window(title='ManipBox', w=200)
-    cmds.columnLayout(adj=True)
-    cmds.separator( h=3 )
-    cmds.text("MinipBox_V2")
-    cmds.separator( h=3 )
-    cmds.button( label = 'World', w=100, h=40, command='wrd()')
+cmds.window(title='ManipBox', w=200)
+cmds.columnLayout(adj=True)
+cmds.separator( h=3 )
+cmds.text("MinipBox_V2")
+cmds.separator( h=3 )
+cmds.button( label = 'World', w=100, h=40, command='wrd()')
 
-    cmds.button( label = 'Object', w=100, h=40, command='lcl()')
+cmds.button( label = 'Object', w=100, h=40, command='lcl()')
 
-    cmds.button( label = 'Custom', w=100, h=40, command='cus()')
+cmds.button( label = 'Custom', w=100, h=40, command='cus()')
 
-    cmds.showWindow()
-
-ManipBox()
+cmds.showWindow()
